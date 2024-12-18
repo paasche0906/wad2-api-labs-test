510bce4d2c66b6c19641ffa4a889356b435c53bb
unknown <jiahchengpan@gmail.com>
2024-12-18 16:56:58 +0000

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler';
 import moviesRouter from './api/movies';   //import movies router
+import authenticate from './authenticate';
 
 
 dotenv.config();
@@ -15,8 +16,10 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use(defaultErrHandler);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
+app.use('/api/movies', authenticate, moviesRouter);
+app.use(defaultErrHandler);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
