7222f0910836f8ef080640769db6b0ab9030cf34
unknown <jiahchengpan@gmail.com>
2024-12-18 16:59:40 +0000

    Added Pagination

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,13 +3,29 @@ import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
     getUpcomingMovies
-  } from '../tmdb-api';
+} from '../tmdb-api';
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
-    const movies = await movieModel.find();
-    res.status(200).json(movies);
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    // Parallel execution of counting movies and getting movies using movieModel
+    const [total_results, results] = await Promise.all([
+        movieModel.estimatedDocumentCount(),
+        movieModel.find().limit(limit).skip((page - 1) * limit)
+    ]);
+    const total_pages = Math.ceil(total_results / limit); //Calculate total number of pages (= total No Docs/Number of docs per page) 
+
+    //construct return Object and insert into response object
+    const returnObject = {
+        page,
+        total_pages,
+        total_results,
+        results
+    };
+    res.status(200).json(returnObject);
 }));
 
 // Get movie details
@@ -19,7 +35,7 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if (movie) {
         res.status(200).json(movie);
     } else {
-        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+        res.status(404).json({ message: 'The movie you requested could not be found.', status_code: 404 });
     }
 }));
 
