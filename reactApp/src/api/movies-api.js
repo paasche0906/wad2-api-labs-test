aeedebb9e6cd5085a9607e042edb8956e65fc0b8
unknown <jiahchengpan@gmail.com>
2024-12-19 11:58:31 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,8 +1,12 @@
 export const getMovies = async () => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=31af661a7adcb32ae044337494e0f685&language=en-US&include_adult=false&page=1`
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
   )
-  return response.json()
+  return response.json();
 };
 
 export const login = async (username, password) => {
